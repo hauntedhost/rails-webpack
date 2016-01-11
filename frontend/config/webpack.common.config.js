cc99d811678991fa3af908f38bcbc33fa6edbe48
Sean Omlor <somlor@eml.cc>
2016-01-11 13:41:00 -0500

    separate rails and webpack configs
    instead of trying to share a .env
    
    - rails configuration is in application.rb as config.webpack
    - main webpack configuration is in webpack.common.config.js

---
+++ frontend/config/webpack.common.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  DEV_SERVER: {
+    PORT: '3030',
+    PATH: 'dev-assets'
+  },
+  DIST_PATH: './dist',
+  ENTRY: {
+    main: './frontend/app/index.js'
+  },
+  FRONTEND_PATH: './frontend/app',
+};
